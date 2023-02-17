af0450af7222cef68970a459f43e102f483c4062
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-02-17 17:03:30 +0400

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Input from "./components/Input";
+import UserInfo from "./components/UserInfo";
 
 function App() {
   return (
     <div className="w-full max-w-[573px] m-auto min-h-screen flex flex-col px-6">
       <Header />
       <Input />
+      <UserInfo />
     </div>
   );
 }
